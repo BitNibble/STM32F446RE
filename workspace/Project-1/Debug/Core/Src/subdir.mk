040e1d7e40f28f3190e5aaa1f0fe8a554ffed794
Sergio Santos <sergi@Victus>
2025-10-18 00:15:16 +0100

    first commit_2

--- workspace/Project-1/Debug/Core/Src/subdir.mk
+++ workspace/Project-1/Debug/Core/Src/subdir.mk
@@ -31,7 +31,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Core/Inc" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/CMSIS/Include" -I"D:/Embedded/STM32-F446RE/workspace/Project-1/Library" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I"D:/Embedded/STM32F446RE/workspace/Project-1/Library" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Core/Inc" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Core-2f-Src
 
--- workspace/Project-1/Debug/Library/subdir.mk
+++ workspace/Project-1/Debug/Library/subdir.mk
@@ -106,7 +106,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Library/%.o Library/%.su Library/%.cyclo: ../Library/%.c Library/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Core/Inc" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Embedded/STM32-F446RE/workspace/Project-2/Drivers/CMSIS/Include" -I"D:/Embedded/STM32-F446RE/workspace/Project-1/Library" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I"D:/Embedded/STM32F446RE/workspace/Project-1/Library" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Core/Inc" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Embedded/STM32F446RE/workspace/Project-1/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Library
 
